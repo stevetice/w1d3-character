ce306c51df9c0d080229f56e1cfc0b18fb811f68
stevetice <tice.steve@gmail.com>
2017-06-28 14:53:11 -0700

    Cleaned up charCount.js and added a couple comments

--- charCount.js
+++ charCount.js
@@ -1,9 +1,3 @@
-// Take input string (lighthouse in the house) => remove spaces
-// Check string for unique values (characters) FIRST
-// if letter does not exist in object add it to key list with value = 1
-// if letter already exists in object add +1 to value
-// Return an object with all key pair values
-
 function countLetters(str) {
   var word = str.split(" ").join("");
   var outputObject = {};
@@ -12,21 +6,11 @@ function countLetters(str) {
     var letter = word[i];
     if (outputObject.hasOwnProperty(letter) !== true)
       outputObject[letter] = 1;
-    // if (outputObject.hasOwnProperty(letter) == letter)
-    //   outputObject[letter] += 1
+    // Check to see if letter exists in outputObject. If it doesn't exist add the letter as a key with a value of 1.
     else
       outputObject[letter] += 1;
-    // console.log(outputObject);
-
-    // if letter is not in output add the letter and outputObject[letter] = 1
-    // if letter already exists in output object then add 1 to count outputObject[letter] ++
-    // console.log(letter);
+    // If letter exists in outputObject we will add 1 to the value.
   }
-
-// console.log(outputObject);
-
-// console.log(outputObject);
-
 return outputObject;
 };
 console.log(countLetters('lighthouse in the house'));
